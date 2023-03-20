9887e865c8d7198e5bf031300f6daf292705e323
ncip48 <mbahcip00@gmail.com>
2023-03-20 11:30:09 +0700

    d

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -8,10 +8,17 @@ class DownloadController extends Controller
 {
     public function download(Request $request)
     {
+        $url = "https://apps-build.berkah-ts.my.id";
         $file_path = public_path('uploads/' . $request->file);
 
         if (file_exists($file_path)) {
-            return response()->download($file_path);
+            return response()->json([
+                'success' => true,
+                'message' => 'File ditemukan!',
+                'data' => [
+                    'file' => $url . '/uploads/' . $request->file
+                ]
+            ]);
         } else {
             return response()->json([
                 'success' => false,
